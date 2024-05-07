d6ef9a4d24b827665bb01522e7479932fc09a1bd
calvinvanoss <cvanoss@calpoly.edu>
2024-05-07 14:06:44 -0700

    stop deleting carts

--- src/api/carts.py
+++ src/api/carts.py
@@ -209,14 +209,6 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
             ),
         )
 
-        connection.execute(
-            sqlalchemy.text(
-                f"""
-                DELETE FROM carts WHERE id = {cart_id};
-                """
-            ),
-        )
-
         return {
             "total_potions_bought": total_potions,
             "total_gold_paid": total_gold,
